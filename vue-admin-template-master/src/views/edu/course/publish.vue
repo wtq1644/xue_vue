9d15afb544988bd4264f7fddb6491f84dec278e4
wtq1644 <uau001798@163.com>
2020-08-08 10:51:56 +0800

    add new file

--- vue-admin-template-master/src/views/edu/course/publish.vue
+++ vue-admin-template-master/src/views/edu/course/publish.vue
@@ -7,33 +7,54 @@
     <el-steps :active="3" process-status="wait" align-center style="margin-bottom: 40px;">
       <el-step title="填写课程基本信息"/>
       <el-step title="创建课程大纲"/>
-      <el-step title="最终发布"/>
+      <el-step title="发布课程"/>
     </el-steps>
 
-    <el-form label-width="120px">
+    <div class="ccInfo">
+      <img :src="coursePublish.cover">
+      <div class="main">
+        <h2>{{ coursePublish.title }}</h2>
+        <p class="gray"><span>共{{ coursePublish.lessonNum }}课时</span></p>
+        <p><span>所属分类：{{ coursePublish.subjectLevelOne }} — {{ coursePublish.subjectLevelTwo }}</span></p>
+        <p>课程讲师：{{ coursePublish.teacherName }}</p>
+        <h3 class="red">￥{{ coursePublish.price }}</h3>
+      </div>
+    </div>
 
-      <el-form-item>
-        <el-button @click="previous">返回修改</el-button>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="publish">发布课程</el-button>
-      </el-form-item>
-    </el-form>
+    <div>
+      <el-button @click="previous">返回修改</el-button>
+      <el-button :disabled="saveBtnDisabled" type="primary" @click="publish">发布课程</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-
+import course from '@/api/edu/course'
 export default {
   data() {
     return {
-      saveBtnDisabled: false // 保存按钮是否禁用
+      saveBtnDisabled: false, // 保存按钮是否禁用
+      courseId:'',
+      coursePublish:{}
     }
   },
 
   created() {
-    console.log('publish created')
+     if(this.$route.params && this.$route.params.id) {
+            this.courseId = this.$route.params.id
+            //调用根据id查询课程的方法
+            this.getCoursePublishId()
+      }
   },
 
   methods: {
+
+    getCoursePublishId(){
+        course.getPublihCourseInfo(this.courseId)
+        .then(response => {
+          this.coursePublish = response.data.publishCourse
+        })
+    },
     previous() {
       console.log('previous')
       this.$router.push({ path: '/course/chapter/1' })
